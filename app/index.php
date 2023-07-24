77df09dc839fb4c3fad018fda022ad30c5e08cc9
Mampionona33 <ramamps33@gmail.com>
2023-07-24 12:44:29 +0000

    finish setup twig

--- app/index.php
+++ app/index.php
@@ -2,6 +2,8 @@
 // Gestion des erreurs probables
 
 use RouterNamespace\Router;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
@@ -10,9 +12,12 @@ ini_set('display_errors', '1');
 require 'vendor/autoload.php';
 
 
+
 final class App
 {
     private $router;
+    private $loader;
+    private $twig;
 
     /**
      * getter and setter
@@ -27,15 +32,44 @@ final class App
         return $this->router;
     }
 
+    public function setTwig(Environment $twig): void
+    {
+        $this->twig = $twig;
+    }
+
+    public function getTwig(): Environment
+    {
+        return $this->twig;
+    }
+
+    public function setLoader(FilesystemLoader $loader): void
+    {
+        $this->loader = $loader;
+    }
+
+    public function getLoader(): FilesystemLoader
+    {
+        return $this->loader;
+    }
+    // --------------------------------------
     public function __construct()
     {
         $this->setRouter(new Router());
+
+        // Créer un loader pour charger les templates depuis un répertoire spécifique (par exemple, 'templates')
+        $loader = new FilesystemLoader(__DIR__ . '/template');
+
+        // Créer l'environnement Twig avec le loader
+        $twig = new Environment($loader);
+
+        // Enregistrez l'instance Twig dans l'objet App
+        $this->setTwig($twig);
     }
 
     public function __invoke()
     {
         $this->getRouter()->get("/", function () {
-            echo  "hello word";
+            echo $this->getTwig()->render('homepage.html.twig');
         });
 
         $this->getRouter()->handleRequest();
