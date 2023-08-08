eb1eb63e9ec77f3aa419a9a4eceb2555ec7065b0
Mampionona <50689325+Mampionona33@users.noreply.github.com>
2023-08-08 11:34:00 +0000

    try to setup authorization

--- app/entity/Authorization.php
+++ app/entity/Authorization.php
@@ -8,6 +8,8 @@ use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\Table;
 
+#[Entity]
+#[Table(name:'Authorization')]
 class Authorization
 {
     #[Id]
